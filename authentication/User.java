10fa7238c3c26e6ed433e6e9d0fec3a125c07502
saicharan <saicharan0054@gmail.com>
2025-09-20 19:18:15 +0530

    User class created

---
+++ authentication/User.java
@@ -0,0 +1,50 @@
+class User
+{
+	private String username;
+	private String password;
+	private long mobile_no;
+	private String e_mail;
+	User(String username,String password,long mobile_no,String e_mail)
+	{
+		this.username=username;
+		this.password=password;
+		this.mobile_no=mobile_no;
+		this.e_mail=e_mail;
+	}
+	void setName(String username)
+	{
+		this.username=username;
+	}
+	void setPass(String password)
+	{
+		this.password=password;
+	}
+	void setMobile(long mobile_no)
+	{
+		this.mobile_no=mobile_no;
+	}
+	void setMail(String e_mail)
+	{
+		this.e_mail=e_mail;
+	}
+	String getName()
+	{
+		return username;
+	}
+	String getPass()
+	{
+		return password;
+	}
+	long getMobile()
+	{
+		return mobile_no
+	}
+	String getMail()
+	{
+		return e_mail
+	}
+	public String toString()
+	{
+		return "Username : "+username+"\nPassword : "+password+"\nMobile number : "+mobile_no+"\nE Mail : "+e_mail;
+	}
+}
